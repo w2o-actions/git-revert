7a34395c67acf9c0ed2d1230a7dbc30ed5202040
Chris Miller <cmiller@w2ogroup.com>
2021-03-17 14:55:57 -0400

    init

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,25 +6,6 @@ GITHUB_TOKEN="635bda25aecdfd9f8ae4ff461c4c169f0725293d"
 # get the SHA to revert
 COMMIT_TO_REVERT=$(git rev-parse HEAD)
 
-PR_NUMBER=$(jq -r ".issue.number" "$GITHUB_EVENT_PATH")
-REPO_FULLNAME=$(jq -r ".repository.full_name" "$GITHUB_EVENT_PATH")
-echo "Collecting information about PR #$PR_NUMBER of $REPO_FULLNAME..."
-
-if [[ -z "$GITHUB_TOKEN" ]]; then
-	echo "Set the GITHUB_TOKEN env variable."
-	exit 1
-fi
-
-URI=https://api.github.com
-API_HEADER="Accept: application/vnd.github.v3+json"
-AUTH_HEADER="Authorization: token $GITHUB_TOKEN"
-
-pr_resp=$(curl -X GET -s -H "${AUTH_HEADER}" -H "${API_HEADER}" \
-          "${URI}/repos/$REPO_FULLNAME/pulls/$PR_NUMBER")
-
-HEAD_REPO=$(echo "$pr_resp" | jq -r .head.repo.full_name)
-HEAD_BRANCH=$(echo "$pr_resp" | jq -r .head.ref)
-
 git remote set-url origin https://x-access-token:$GITHUB_TOKEN@github.com/$REPO_FULLNAME.git
 git config --global user.email "revert@github.com"
 git config --global user.name "GitHub Revert Action"
@@ -38,5 +19,5 @@ git checkout -b $HEAD_BRANCH origin/$HEAD_BRANCH
 
 # check commit exists
 git cat-file -t $COMMIT_TO_REVERT
-git revert $COMMIT_TO_REVERT --no-edit
-git push origin $HEAD_BRANCH
+git revert $COMMIT_TO_REVERT -m 1 --no-edit
+git push origin
