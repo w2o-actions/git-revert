8d9b543f25b374fafb8d9e853f5dda9dc1a1d890
Chris Miller <cmiller@w2ogroup.com>
2021-03-18 09:00:21 -0400

    tweaks

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,8 @@ GITHUB_TOKEN="635bda25aecdfd9f8ae4ff461c4c169f0725293d"
 
 # get the SHA to revert
 COMMIT_TO_REVERT=$(git rev-parse HEAD)
+# get this branch
+HEAD_BRANCH=$(git branch --show-current)
 
 git remote set-url origin https://x-access-token:$GITHUB_TOKEN@github.com/$REPO_FULLNAME.git
 git config --global user.email "revert@github.com"
@@ -12,14 +14,19 @@ git config --global user.name "GitHub Revert Action"
 
 set -o xtrace
 
-HEAD_BRANCH=$(git branch --show-current)
+
 
 git fetch origin $HEAD_BRANCH
 
-# do the revert
-git checkout -b $HEAD_BRANCH origin/$HEAD_BRANCH
+
+#git checkout -b $HEAD_BRANCH origin/$HEAD_BRANCH
 
 # check commit exists
 git cat-file -t $COMMIT_TO_REVERT
+
+# do the revert
 git revert $COMMIT_TO_REVERT -m 1 --no-edit
+# pass commit message in so we can identify what step failed
+
+git commit --amend -m $COMMIT_MESSAGE
 git push origin
