718b69e646a4bc802f040630a117c2f3ae0b6cfc
GitHub Revert Action <revert@github.com>
2021-03-18 11:21:51 -0400

    updates for resetting parent of merged branch to mitigate conflicts

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,25 +1,23 @@
 #!/bin/bash
 
 set -e
-GITHUB_TOKEN="635bda25aecdfd9f8ae4ff461c4c169f0725293d"
 
 # get the SHA to revert
 COMMIT_TO_REVERT=$(git rev-parse HEAD)
 # get this branch
 HEAD_BRANCH=$(git branch --show-current)
 
+# get the branch that was just merged
+MERGED_BRANCH=$(git log --merges origin/$HEAD_BRANCH --oneline --grep="^Merge pull request #\([0-9]\+\)" -1 | awk -F"$REPO_OWNER/" ' { print $NF } ')
+
+# set git config
 git remote set-url origin https://x-access-token:$GITHUB_TOKEN@github.com/$REPO_FULLNAME.git
 git config --global user.email "revert@github.com"
 git config --global user.name "GitHub Revert Action"
 
 set -o xtrace
 
-
-
 git fetch origin $HEAD_BRANCH
-
-
-#git checkout -b $HEAD_BRANCH origin/$HEAD_BRANCH
 
 # check commit exists
 git cat-file -t $COMMIT_TO_REVERT
@@ -30,3 +28,9 @@ git revert $COMMIT_TO_REVERT -m 1 --no-edit
 
 git commit --amend -m "$COMMIT_MESSAGE"
 git push origin
+
+
+git fetch && git checkout $MERGED_BRANCH
+git add -A
+git commit -m "reset parent to revert commit -- due to $COMMIT_MESSAGE"
+git push
