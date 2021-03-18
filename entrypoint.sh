80b943e101ba05aceb025a1f2e75a32d1da559ea
GitHub Revert Action <revert@github.com>
2021-03-18 11:50:45 -0400

    more tweaking

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+echo $COMMIT_MESSAGE
 # get the SHA to revert
 COMMIT_TO_REVERT=$(git rev-parse HEAD)
 # get this branch
@@ -11,9 +12,9 @@ HEAD_BRANCH=$(git branch --show-current)
 MERGED_BRANCH=$(git log --merges origin/$HEAD_BRANCH --oneline --grep="^Merge pull request #\([0-9]\+\)" -1 | awk -F"$REPO_OWNER/" ' { print $NF } ')
 
 # set git config
-git remote set-url origin https://x-access-token:$GITHUB_TOKEN@github.com/$REPO_FULLNAME.git
-git config --global user.email "revert@github.com"
-git config --global user.name "GitHub Revert Action"
+# git remote set-url origin https://x-access-token:$GITHUB_TOKEN@github.com/$REPO_FULLNAME.git
+# git config --global user.email "revert@github.com"
+# git config --global user.name "GitHub Revert Action"
 
 set -o xtrace
 
@@ -27,7 +28,7 @@ git revert $COMMIT_TO_REVERT -m 1 --no-edit
 # pass commit message in so we can identify what step failed
 
 git commit --amend -m "$COMMIT_MESSAGE"
-git push origin
+git push -u origin $HEAD_BRANCH
 
 echo "merged branch --> $MERGED_BRANCH"
 git fetch
@@ -36,4 +37,4 @@ git branch
 git pull origin $HEAD_BRANCH --quiet
 git add -A
 git commit -m "reset parent to revert commit -- due to $COMMIT_MESSAGE"
-git push
+git push -u origin $MERGED_BRANCH
